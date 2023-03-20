2a714d5bf1f71852dffdc60a085d5a4a46d61ef1
Hussein <husseinfaqih@gmail.com>
2023-03-20 16:18:59 +0100

    included localStorage

--- src/components/CharacterIDContext.js
+++ src/components/CharacterIDContext.js
@@ -1,10 +1,17 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const CharacterIDContext = createContext();
 
 export const CharacterIDProvider = (props) => {
-  const [favorite, setFavorite] = useState([]);
-  console.log(favorite);
+  const [favorite, setFavorite] = useState(() => {
+    const localData = localStorage.getItem('favorite');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('favorite', JSON.stringify(favorite));
+    console.log(favorite);
+  }, [favorite]);
 
   return (
     <CharacterIDContext.Provider value={{ favorite, setFavorite }}>
